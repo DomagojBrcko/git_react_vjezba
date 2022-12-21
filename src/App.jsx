2121e65bd4e2ad4190aeac3d3016ed44194ffe3f
Domagoj <db@gmil.com>
2022-12-21 11:14:21 +0100

    Vježba

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
     <div>
       {a%2 === 0 && <p>Broj {a} je paran</p>}
       {a%2 !== 0 && <p>Broj {a} je neparan</p>}
+      {a === 0 && <p>Broj je nula.</p>}
       
 
     </div>
